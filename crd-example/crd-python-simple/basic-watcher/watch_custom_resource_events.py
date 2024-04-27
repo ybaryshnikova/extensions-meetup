fd5b423ccf3926b86d7b918b481ba58453575c77
Yuliya Baryshnikava <yuliya.baryshnikava@integritynext.com>
2024-04-27 15:38:29 +0300

    revise simple controller

--- crd-example/crd-python-simple/basic-watcher/watch_custom_resource_events.py
+++ crd-example/crd-python-simple/basic-watcher/watch_custom_resource_events.py
@@ -1,7 +1,11 @@
 from kubernetes import client, config, watch
+import signal
+import os
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
+# Use in-cluster config instead of kube_config
+# config.load_incluster_config()
 
 # Define the custom resource details
 group = 'crd.dev'
@@ -15,15 +19,30 @@ api = client.CustomObjectsApi()
 
 
 def watch_custom_resource_events():
-
-    # Watch the custom resource events
     w = watch.Watch()
-    for event in w.stream(api.list_namespaced_custom_object, group, version, namespace, resource_plural, pretty='true'):
-        # Print the event type and the name of the custom resource
-        event_type = event['type']  # ADDED, MODIFIED, DELETED
-        resource_name = event['object']['metadata']['name']
-        print(f"Event: {event_type}, Name: {resource_name}")
-        print(f"Event: {event}")
+    try:
+        # Setup a signal handler to stop watching when receiving SIGINT or SIGTERM
+        def stop_watch(signum, frame):
+            print("Stopping the watch...")
+            w.stop()
+            print("Stopped")
+            os._exit(0)
+
+        signal.signal(signal.SIGINT, stop_watch)
+        signal.signal(signal.SIGTERM, stop_watch)
+
+        for event in w.stream(api.list_namespaced_custom_object, group, version, namespace, resource_plural, pretty='true'):
+            # Print the event type and the name of the custom resource
+            event_type = event['type']  # ADDED, MODIFIED, DELETED
+            resource_name = event['object']['metadata']['name']
+            print(f"Event: {event_type}, Name: {resource_name}")
+            print(f"Event: {event}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        w.stop()
+        print("Watch has been stopped.")
 
 
 if __name__ == '__main__':
